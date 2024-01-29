be51c34bba9aa635303a91a1ea73c838b04fec45
Ben Menko <benjamin.menko@gmail.com>
2024-01-29 18:21:34 +0000

    reset uppercase.S

--- hw0/uppercase.disasm.s
+++ hw0/uppercase.disasm.s
@@ -5,9 +5,23 @@ uppercase.bin:     file format elf32-littleriscv
 Disassembly of section .text:
 
 00010074 <_start>:
-   10074:	ffff2517          	auipc	a0,0xffff2
-   10078:	f8c50513          	addi	a0,a0,-116 # 2000 <__DATA_BEGIN__>
-   1007c:	00052503          	lw	a0,0(a0)
+   10074:	ffff2297          	auipc	t0,0xffff2
+   10078:	f8c28293          	addi	t0,t0,-116 # 2000 <__DATA_BEGIN__>
 
-00010080 <end_program>:
-   10080:	0000006f          	j	10080 <end_program>
+0001007c <first_process>:
+   1007c:	00028303          	lb	t1,0(t0)
+   10080:	02030463          	beqz	t1,100a8 <end_program>
+   10084:	06100393          	li	t2,97
+   10088:	00734a63          	blt	t1,t2,1009c <second_process>
+   1008c:	07a00393          	li	t2,122
+   10090:	0063c663          	blt	t2,t1,1009c <second_process>
+   10094:	fe030313          	addi	t1,t1,-32
+   10098:	0040006f          	j	1009c <second_process>
+
+0001009c <second_process>:
+   1009c:	00628023          	sb	t1,0(t0)
+   100a0:	00128293          	addi	t0,t0,1
+   100a4:	fd9ff06f          	j	1007c <first_process>
+
+000100a8 <end_program>:
+   100a8:	0000006f          	j	100a8 <end_program>
