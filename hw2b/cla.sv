a26d9d57e2dbab570bd86ce9c98d72e510243c43
Ben Menko <benjamin.menko@gmail.com>
2024-02-19 02:03:53 +0000

    working on cla

--- hw2b/cla.sv
+++ hw2b/cla.sv
@@ -27,7 +27,6 @@ module gp4(input wire [3:0] gin, pin,
            output wire gout, pout,
            output wire [2:0] cout);
 
-   // TODO: your code here
    logic c1, c2, c3, p_out, g_out;
    always_comb begin
       c1 = (gin[0] | (pin[0] & cin));
@@ -49,7 +48,6 @@ module gp8(input wire [7:0] gin, pin,
            input wire cin,
            output wire gout, pout,
            output wire [6:0] cout);
-   // TODO: your code here
    logic c1, c2, c3, c4, c5, c6, c7, g_out, p_out;
    always_comb begin
       c1 = (gin[0] | (pin[0] & cin));
@@ -85,42 +83,61 @@ module cla
    genvar i;
    generate
       for (i = 0; i < 32; i = i + 1) begin
-         gp1 bit1(.a(a[i]),
-                  .b(b[i]), 
-                  .g(level_one_g[i]), 
-                  .p(level_one_p[i]));
-      end
-   endgenerate
-
-   // 8 4-bit modules
-   wire level_two_g[8];
-   wire level_two_p[8];
-   wire [2:0] level_two_c[8];
-   genvar j;
-   generate
-      for (j = 0; j < 8; j = j + 1) begin
-         gp4 bit4(.gin(level_one_g[4*j+3:4*j]),
-                  .pin(level_one_p[4*j+3:4*j]), 
-                  .cin(cin), 
-                  .gout(level_two_g[j]),
-                  .pout(level_two_p[j]),
-                  .cout(level_two_c[j]))
+         gp1 bit1(.a(a[i]), .b(b[i]), .g(level_one_g[i]), .p(level_one_p[i]));
       end
    endgenerate
 
    // 4 8-bit modules
-   wire level_three_g[4];
-   wire level_three_p[4];
-   wire [6:0] level_three_c[4];
+   wire level_two_g[4];
+   wire level_two_p[4];
+   wire [6:0] level_two_c[4];
    genvar k;
    generate
       for (k = 0; k < 4; k = k + 1) begin
-         gp8 bit8(.gin(level_two_g[8*k+7 : 8*k]),
-                  .pin(level_two_p[8*k+7 : 8*k]), 
-                  .cin(cin), 
-                  .gout(level_three_g[k]),
-                  .pout(level_three_p[k]),
-                  .cout(level_three_c[k]))
+         gp8 bit8(.gin(level_one_g[8*k+7 : 8*k]), .pin(level_one_p[8*k+7 : 8*k]), .cin(cin), .gout(level_two_g[k]), .pout(level_two_p[k]),  .cout(level_two_c[k]));
       end
    endgenerate
+
+   logic [31:0] c;
+   always_comb begin
+      c[0] = level_two_c[0][0];
+      c[1] = level_two_c[0][1];
+      c[2] = level_two_c[0][2];
+      c[3] = level_two_c[0][3];
+      c[4] = level_two_c[0][4];
+      c[5] = level_two_c[0][5];
+      c[6] = level_two_c[0][6];
+      c[7] = (cin & level_two_p[0]) | level_two_g[0];
+      c[8] = level_two_c[1][0];
+      c[9] = level_two_c[1][1];
+      c[10] = level_two_c[1][2];
+      c[11] = level_two_c[1][3];
+      c[12] = level_two_c[1][4];
+      c[13] = level_two_c[1][5];
+      c[14] = level_two_c[1][6];
+      c[15] = (c[7] & level_two_p[1]) | level_two_g[1];
+      c[16] = level_two_c[2][0];
+      c[17] = level_two_c[2][1];
+      c[18] = level_two_c[2][2];
+      c[19] = level_two_c[2][3];
+      c[20] = level_two_c[2][4];
+      c[21] = level_two_c[2][5];
+      c[22] = level_two_c[2][6];
+      c[23] = (c[15] & level_two_p[2]) | level_two_g[2];
+      c[24] = level_two_c[3][0];
+      c[25] = level_two_c[3][1];
+      c[26] = level_two_c[3][2];
+      c[27] = level_two_c[3][3];
+      c[28] = level_two_c[3][4];
+      c[29] = level_two_c[3][5];
+      c[30] = level_two_c[3][6];
+      c[31] = (c[23] & level_two_p[3]) | level_two_g[3];
+   end
+
+   genvar l;
+   generate
+      for (l = 0; l < 32; l = l + 1) begin
+         assign sum[l] = a[l] ^ b[l] ^ c[l];
+      end
+   endgenerate 
 endmodule
